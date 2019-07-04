6416459d645c75070fc1c11d61fc930365f0d675
Wilfred Hughes <me@wilfred.me.uk>
2019-07-05 00:37:49 +0100

    Basic template and styling

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const commonmark = require("commonmark");
 const Koa = require("koa");
 const Router = require("koa-router");
+const Handlebars = require("handlebars");
 
 const app = new Koa();
 const router = new Router();
 
 var fs = require("fs"),
   path = require("path"),
-  filePath = path.join(__dirname, "concept.md");
+  filePath = path.join(__dirname, "concept.md"),
+  indexTemplatePath = path.join(__dirname, "templates/index.html");
 
 const homePageSrc = fs.readFileSync(filePath, { encoding: "utf8" });
+const indexTemplate = Handlebars.compile(
+  fs.readFileSync(indexTemplatePath, { encoding: "utf8" })
+);
 
 function renderMarkdown(src) {
   var reader = new commonmark.Parser();
@@ -22,7 +27,7 @@ function renderMarkdown(src) {
 }
 
 router.get("/", (ctx, next) => {
-  ctx.body = renderMarkdown(homePageSrc);
+  ctx.body = indexTemplate({ content: renderMarkdown(homePageSrc) });
 });
 
 app.use(router.routes()).use(router.allowedMethods());
