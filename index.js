748e06e51ecaed5b1799c6c0e23ed428ba6a2771
Wilfred Hughes <me@wilfred.me.uk>
2019-07-04 10:10:09 +0100

    Markdown and URL router

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const commonmark = require("commonmark");
 const Koa = require("koa");
-const app = new Koa();
+const Router = require("koa-router");
 
-// response
-app.use(ctx => {
-  ctx.body = "Hello Koa";
+const app = new Koa();
+const router = new Router();
+
+function renderMarkdown(src) {
+  var reader = new commonmark.Parser();
+  var writer = new commonmark.HtmlRenderer();
+  // parsed is a 'Node' tree
+  var parsed = reader.parse(src);
+
+  // transform parsed if you like...
+  return writer.render(parsed);
+}
+
+router.get("/", (ctx, next) => {
+  ctx.body = renderMarkdown("Hello **world**");
 });
 
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(3000);
+console.log("Server listening on 3000");
