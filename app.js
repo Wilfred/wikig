09bd0d95310469a7b3df4906391a68d79682639a
Wilfred Hughes <me@wilfred.me.uk>
2019-07-14 23:03:48 +0100

    Separate routes from app

--- app.js
+++ app.js
@@ -1,71 +1,21 @@
-const commonmark = require("commonmark");
-const wikiWordsTransform = require("commonmark-wikiwords");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
 const path = require("path");
 
-const db = require("./db");
+const indexRouter = require("./routes/index");
+const pageRouter = require("./routes/pages");
 
 const app = express();
 
 app.engine(".html", exphbs({ extname: ".html" }));
 app.set("view engine", ".html");
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-function renderMarkdown(src) {
-  const reader = new commonmark.Parser();
-  const writer = new commonmark.HtmlRenderer();
-  // parsed is a 'Node' tree
-  const parsed = reader.parse(src);
-
-  return writer.render(wikiWordsTransform(parsed));
-}
-
 app.use(
   "/static/semantic/",
   express.static(path.join(__dirname, "node_modules", "semantic-ui-css"))
 );
 
-app.get("/", (req, res) => res.redirect("/page/HomePage"));
-app.get("/page/:name", (req, res) => {
-  const name = req.params.name;
-  db.getPage(name, (err, page) => {
-    if (page) {
-      return res.render("page", {
-        subtitle: "| " + name,
-        title: name,
-        content: renderMarkdown(page.content)
-      });
-    }
-    res.send("no such page: " + name);
-  });
-});
-
-app.get("/edit/:name", (req, res) => {
-  const name = req.params.name;
-  db.getPage(name, (err, page) => {
-    if (page) {
-      return res.render("edit", {
-        subtitle: "| " + name,
-        title: name,
-        content: page.content
-      });
-    }
-    res.send("no such page: " + name);
-  });
-});
-
-app.post("/edit/:name", urlencodedParser, (req, res) => {
-  // Prefer the POST parameter to the URL, so we can rename pages.
-  const name = req.body.title;
-  db.updatePage(name, req.body.content, (err, _page) => {
-    if (err) {
-      console.error(err);
-    }
-    res.redirect("/page/" + name);
-  });
-});
+app.use("/", indexRouter);
+app.use("/", pageRouter);
 
 module.exports = app;
---
+++ routes/pages.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const commonmark = require("commonmark");
+const wikiWordsTransform = require("commonmark-wikiwords");
+
+const db = require("../db");
+
+const router = express.Router();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+function renderMarkdown(src) {
+  const reader = new commonmark.Parser();
+  const writer = new commonmark.HtmlRenderer();
+  // parsed is a 'Node' tree
+  const parsed = reader.parse(src);
+
+  return writer.render(wikiWordsTransform(parsed));
+}
+
+router.get("/page/:name", (req, res) => {
+  const name = req.params.name;
+  db.getPage(name, (err, page) => {
+    if (page) {
+      return res.render("page", {
+        subtitle: "| " + name,
+        title: name,
+        content: renderMarkdown(page.content)
+      });
+    }
+    res.send("no such page: " + name);
+  });
+});
+
+router.get("/edit/:name", (req, res) => {
+  const name = req.params.name;
+  db.getPage(name, (err, page) => {
+    if (page) {
+      return res.render("edit", {
+        subtitle: "| " + name,
+        title: name,
+        content: page.content
+      });
+    }
+    res.send("no such page: " + name);
+  });
+});
+
+router.post("/edit/:name", urlencodedParser, (req, res) => {
+  // Prefer the POST parameter to the URL, so we can rename pages.
+  const name = req.body.title;
+  db.updatePage(name, req.body.content, (err, _page) => {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect("/page/" + name);
+  });
+});
+
+module.exports = router;
