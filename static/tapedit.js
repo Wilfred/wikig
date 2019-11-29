103ca048e8e331df317fdd7b823b97418f0fea0d
Wilfred Hughes <me@wilfred.me.uk>
2019-11-29 10:27:42 +0000

    Silence eslint warnings for frontend JS

--- static/tapedit.js
+++ static/tapedit.js
@@ -1,12 +1,18 @@
-var headers = document.getElementsByTagName("h1");
+/* Navigate to the edit URL if double clicking or double tapping on
+ * the page title. */
 
-console.log(headers);
+/* This is run client side, so target a wide range of older
+ * browsers. */
+/* eslint-env browser */
+/* eslint no-var: "off", prefer-arrow-callback: "off" */
+/* global Hammer */
+var headers = document.getElementsByTagName("h1");
 
 for (var header of headers) {
   if (header.hasAttribute("data-edit-url")) {
     var url = header.getAttribute("data-edit-url");
     var h = new Hammer(header);
-    h.on("doubletap", function(ev) {
+    h.on("doubletap", function(_ev) {
       window.location.href = url;
     });
   }
