5908f04c2f1e58faef18eac77c0daf87fc1ca911
Wilfred Hughes <me@wilfred.me.uk>
2020-06-12 23:36:54 -0700

    Link to the current git commit

--- routes/version.js
+++ routes/version.js
@@ -2,6 +2,7 @@ const moment = require("moment");
 const express = require("express");
 const os = require("os");
 const emoji = require("../lib/emoji");
+const execSync = require("child_process").execSync;
 const router = express.Router();
 
 router.get("/version", (req, res) => {
@@ -10,10 +11,15 @@ router.get("/version", (req, res) => {
     .humanize();
   const serverUptime = moment.duration(-1 * os.uptime(), "seconds").humanize();
 
+  const commit = execSync("git rev-parse HEAD").toString();
+  const commitUrl = `https://github.com/Wilfred/wikig/commit/${commit}`;
+
   return res.render("version", {
     title: "Site Version",
     emoji: emoji.render("⚙️"),
     version: process.versions.node,
+    commit: commit.substring(0, 8),
+    commitUrl,
     appUptime,
     serverUptime
   });
