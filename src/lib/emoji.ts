627a1dabb91533d0ce9dbfef2d43ad3bade083de
Wilfred Hughes <me@wilfred.me.uk>
2025-02-09 23:46:01 -0800

    Add types in emoji.js

--- src/lib/emoji.ts
+++ src/lib/emoji.ts
@@ -1,66 +1,18 @@
-"use strict";
-var __createBinding =
-  (this && this.__createBinding) ||
-  (Object.create
-    ? function (o, m, k, k2) {
-        if (k2 === undefined) k2 = k;
-        var desc = Object.getOwnPropertyDescriptor(m, k);
-        if (
-          !desc ||
-          ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)
-        ) {
-          desc = {
-            enumerable: true,
-            get: function () {
-              return m[k];
-            },
-          };
-        }
-        Object.defineProperty(o, k2, desc);
-      }
-    : function (o, m, k, k2) {
-        if (k2 === undefined) k2 = k;
-        o[k2] = m[k];
-      });
-var __setModuleDefault =
-  (this && this.__setModuleDefault) ||
-  (Object.create
-    ? function (o, v) {
-        Object.defineProperty(o, "default", { enumerable: true, value: v });
-      }
-    : function (o, v) {
-        o["default"] = v;
-      });
-var __importStar =
-  (this && this.__importStar) ||
-  (function () {
-    var ownKeys = function (o) {
-      ownKeys =
-        Object.getOwnPropertyNames ||
-        function (o) {
-          var ar = [];
-          for (var k in o)
-            if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-          return ar;
-        };
-      return ownKeys(o);
-    };
-    return function (mod) {
-      if (mod && mod.__esModule) return mod;
-      var result = {};
-      if (mod != null)
-        for (var k = ownKeys(mod), i = 0; i < k.length; i++)
-          if (k[i] !== "default") __createBinding(result, mod, k[i]);
-      __setModuleDefault(result, mod);
-      return result;
-    };
-  })();
-Object.defineProperty(exports, "__esModule", { value: true });
-const _ = __importStar(require("lodash"));
+import * as _ from "lodash";
 const emojilib = require("emojilib");
 const nlp = require("compromise");
 const twemoji = require("twemoji");
-const WORD_BLACKLIST = [
+
+type Emoji = {
+  keywords: string[];
+  char: string;
+  fitzpatrick_scale: boolean;
+  category: string;
+};
+
+type LabelledEmoji = Emoji & { key: string; target: string };
+
+const WORD_BLACKLIST: string[] = [
   "data",
   "enter", // boring arrow
   "express",
@@ -77,7 +29,8 @@ const WORD_BLACKLIST = [
   "what", // surprised face
   "yes",
 ];
-const KEYWORD_BLACKLIST = [
+
+const KEYWORD_BLACKLIST: string[] = [
   // We sometimes find valid emoji like 'have' mapping to 'u6709' but
   // it doesn't make sense for english readers.
   "chinese",
@@ -90,10 +43,14 @@ const KEYWORD_BLACKLIST = [
   // Macedonia).
   "flag",
 ];
-const EMOJI_BLACKLIST = [
+
+const EMOJI_BLACKLIST: string[] = [
   "radio_button", // associated with 'old'
 ];
-const REPLACEMENTS = {
+
+const REPLACEMENTS: {
+  [key: string]: string;
+} = {
   // The only emojis associated with 'program' are 'radio' and 'tv'.
   program: "programmer",
   // Since we end up using 'male_technologist' for programmer, use
@@ -105,29 +62,40 @@ const REPLACEMENTS = {
   // The only emoji with 'tree' as a keyword is 'leaves'.
   tree: "deciduous_tree",
 };
+
 // Return true if `haystack` contains any of the values in `needles`.
-function containsAny(haystack, needles) {
+function containsAny(haystack: string[], needles: string[]): boolean {
   return !_.isEmpty(_.intersection(haystack, needles));
 }
+
 // All the emoji in emojilib that we want to use.
-function relevantEmoji() {
+function relevantEmoji(): { [key: string]: Emoji } {
   let result = emojilib.lib;
+
   // Ignore emoji whose name is in the blacklist.
   result = _.omit(result, EMOJI_BLACKLIST);
+
   // Ignore emoji if any keywords are in the blacklist.
-  result = _.omitBy(result, (value) =>
+  result = _.omitBy(result, (value: Emoji) =>
     containsAny(value.keywords, KEYWORD_BLACKLIST),
   );
+
   return result;
 }
-function keywordMap(emojiMap) {
-  const result = {};
+
+function keywordMap(emojiMap: { [key: string]: Emoji }): {
+  [key: string]: Emoji & { key: string };
+} {
+  const result: { [key: string]: Emoji & { key: string } } = {};
+
   // TODO: Iterate object more nicely.
   Object.keys(emojiMap).forEach((key) => {
     // TODO: Prefer built-in methods.
     const value = _.extend({ key }, emojiMap[key]);
-    value.keywords.forEach((keyword) => {
+
+    value.keywords.forEach((keyword: string) => {
       const prev = result[keyword];
+
       // Prefer emoji with fewer keywords as they're usually a better
       // match. For example, 'home' matches family emoji, which have
       // lots of keywords, when we really want a building.
@@ -136,40 +104,56 @@ function keywordMap(emojiMap) {
       }
     });
   });
+
   return result;
 }
-function prefixMap(emojiMap) {
+
+function prefixMap(emojiMap: { [key: string]: Emoji }): {
+  [key: string]: Emoji & { key: string };
+} {
   // If we still haven't found something, try splitting names. This is
   // useful for matching 'page_facing_u' to 'page'.
-  const result = {};
+
+  const result: {
+    [key: string]: Emoji & { key: string };
+  } = {};
+
   // TODO: Iterate object more nicely.
   Object.keys(emojiMap).forEach((key) => {
     // TODO: Prefer built-in methods.
     const value = _.extend({ key }, emojiMap[key]);
+
     // Only use the first part, so 'call_me_hand' doesn't match
     // 'me'.
     const prefix = key.split("_")[0];
+
     if (prefix.length > 1) {
       result[prefix] = value;
     }
   });
+
   return result;
 }
-const ALL_EMOJI = relevantEmoji();
+
+const ALL_EMOJI: { [key: string]: Emoji } = relevantEmoji();
 const EMOJI_BY_KEYWORD = keywordMap(ALL_EMOJI);
 const EMOJI_BY_PREFIX = prefixMap(ALL_EMOJI);
-function emojiMatchingKeyword(originalTarget) {
+
+function emojiMatchingKeyword(originalTarget: string) {
   const target = REPLACEMENTS[originalTarget] || originalTarget;
+
   // If there's an emoji with exactly this name, use it.
   const exactMatch = ALL_EMOJI[target];
   if (exactMatch != null) {
     return _.extend({ key: target, target: originalTarget }, exactMatch);
   }
+
   // Otherwise, try to find an emoji that contains this word as a keyword.
   const keywordMatch = EMOJI_BY_KEYWORD[target];
   if (keywordMatch != null) {
     return _.extend({ key: target, target: originalTarget }, keywordMatch);
   }
+
   // If we still haven't found something, try splitting names.
   // useful for matching 'page_facing_u' to 'page'. Don't consider
   // very short targets, as 'do' or 'no' isn't relevant.
@@ -178,39 +162,49 @@ function emojiMatchingKeyword(originalTarget) {
     const prefixMatch = EMOJI_BY_PREFIX[target];
     return _.extend({ key: target, target: originalTarget }, prefixMatch);
   }
+
   return null;
 }
-function findEmoji(words) {
-  const result = [];
-  _.forEach(words, (word) => {
+
+function findEmoji(words: string[]): { target: string; key: string }[] {
+  const result: { target: string; key: string }[] = [];
+  _.forEach(words, (word: string) => {
     if (word.length < 3 || WORD_BLACKLIST.includes(word.toLowerCase())) {
       return true;
     }
+
     const emoji = emojiMatchingKeyword(word.toLowerCase());
+
     if (emoji && !result.includes(emoji)) {
       result.push(emoji);
     }
   });
+
   return result;
 }
-function words(txt) {
+
+function words(txt: string): string[] {
   return nlp(txt)
     .normalize({ plurals: true, verbs: true })
     .terms()
     .out("array");
 }
-function findWordEmoji(src) {
+
+function findWordEmoji(src: string) {
   return findEmoji(words(src));
 }
+
 /** Replace emojis in emojiStr with img tags.
  */
-function render(emojiStr) {
+function render(emojiStr: string | null): string | null {
   if (!emojiStr) {
     return null;
   }
+
   return twemoji.parse(emojiStr, {
     base: "/static/twemoji/",
     folder: "72x72",
   });
 }
+
 module.exports = { findWordEmoji, findEmoji, render };
